d6b729f777dbff765eb735d0fa11bfe1f548db7a
Alexander Morozov <alexandr.v.mzv@gmail.com>
2023-07-23 14:37:11 +0600

    made a layout and a card without chips

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,145 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  export let name: string;
+  import placeholder from "./assets/placeholder.svg";
 </script>
 
 <main>
-  <img src={svelteLogo} alt="Svelte Logo" />
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <div class="layout">
+    <div class="list-screen">
+      <div class="card-list">
+        <div class="card">
+          <img class="image" src={placeholder} alt="card" />
+          <div class="description">
+            <div class="origin">Guanacaste, Costa Rica</div>
+            <div class="blend-name">Hello Bean</div>
+            <div class="variety">Kaffa</div>
+          </div>
+        </div>
+        <!--        <div class="card">-->
+        <!--          <img class="image" src={placeholder} alt="card" />-->
+        <!--          <div class="description">-->
+        <!--            <div class="origin">Guanacaste, Costa Rica</div>-->
+        <!--            <div class="blend-name">Hello Bean</div>-->
+        <!--            <div class="variety">Kaffa</div>-->
+        <!--          </div>-->
+        <!--        </div>-->
+        <!--        <div class="card">-->
+        <!--          <img class="image" src={placeholder} alt="card" />-->
+        <!--          <div class="description">-->
+        <!--            <div class="origin">Guanacaste, Costa Rica</div>-->
+        <!--            <div class="blend-name">Hello Bean</div>-->
+        <!--            <div class="variety">Kaffa</div>-->
+        <!--          </div>-->
+        <!--        </div>-->
+      </div>
+      <button class="add-button">+</button>
+    </div>
+  </div>
 </main>
 
-<style>
+<style lang="less">
+  @mobile-screen-size: 600px;
+
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    overflow-y: auto;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .layout {
+    padding: 60px 8px;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+  @media (max-width: @mobile-screen-size) {
+    .layout {
+      padding: 30px 8px;
     }
+  }
+
+  .list-screen {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+  }
+
+  @media (max-width: @mobile-screen-size) {
+    .list-screen {
+      gap: 20px;
+    }
+  }
+
+  .card-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+  }
+
+  @media (max-width: @mobile-screen-size) {
+    .card-list {
+      gap: 10px;
+    }
+  }
+
+  .card {
+    position: relative;
+    width: 500px;
+    /* todo временно проставил height, убрать */
+    height: 449px;
+    background-color: white;
+  }
+
+  @media (max-width: @mobile-screen-size) {
+    .card {
+      width: 304px;
+    }
+  }
+
+  .image {
+    width: 500px;
+    height: 242px;
+    object-fit: cover;
+    border-bottom: 1px solid #d9d9d9;
+  }
+
+  @media (max-width: @mobile-screen-size) {
+    .image {
+      width: 304px;
+    }
+  }
+
+  .description {
+    padding: 24px 0 24px 20px;
+
+    .origin {
+      color: #6d6d6d;
+      margin-bottom: 16px;
+    }
+
+    .blend-name {
+      margin-bottom: 8px;
+      font-size: 24px;
+      font-weight: 500;
+      line-height: 29px;
+    }
+
+    .variety {
+      margin-bottom: 20px;
+    }
+  }
+
+  .add-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80px;
+    height: 80px;
+
+    border-radius: 50%;
+    border: none;
+    font-size: 40px;
+    background-color: #0c0c0e;
+    color: #ececec;
   }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import "./global.css";
+import "./styles/reset.css";
+import "./styles/global.css";
 
 import App from "./App.svelte";
 
 const app = new App({
   target: document.body,
-  props: {
-    name: "world",
-  },
 });
 
 export default app;
